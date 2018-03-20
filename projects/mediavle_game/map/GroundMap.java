f966b98d275544ac7a80bd95080cc03819a8ae7a
Unknown <finn.eggers@gmx.de>
2018-03-20 23:47:57 +0100

    ehm bug I guess

--- projects/mediavle_game/map/GroundMap.java
+++ projects/mediavle_game/map/GroundMap.java
@@ -15,11 +15,11 @@ public class GroundMap extends UniqueGameEntity {
     Field[][] fields;
 
     public GroundMap(int fields_x, int fields_y) {
-        super(0,0,fields_x,fields_y);
+        super(0, 0, fields_x, fields_y);
         fields = new Field[fields_x][fields_y];
 
-        for(int i = 0; i < fields_x; i++) {
-            for(int n = 0;n < fields_y; n++) {
+        for (int i = 0; i < fields_x; i++) {
+            for (int n = 0; n < fields_y; n++) {
                 fields[i][n] = new Field();
             }
         }
@@ -38,9 +38,8 @@ public class GroundMap extends UniqueGameEntity {
     private void initModels() {
         this.generateTexturedModel();
         this.generateEntity();
-        this.entity.setScale(this.width,1, this.getHeight());
-        this.entity.setPosition(0,0,this.width);
-
+        this.entity.setScale(this.width, 1, this.getHeight());
+        this.entity.setPosition(0, 0, this.width);
 
 
         /**
@@ -51,12 +50,10 @@ public class GroundMap extends UniqueGameEntity {
     }
 
     public boolean rigidBody(float x, float z) {
-<<<<<<< HEAD
-        if(x > 0 && z > 0 && x < this.width && z < this.height && fields[(int)x][(int)z].getGameEntity() != null){
-=======
-        if(x > 0 && z > 0 && x < this.width && z < this.height){
->>>>>>> Finn
-            if(fields[(int)x][(int)z].getGameEntity().isRigidBody()) return true;
+        if (x > 0 && z > 0 && x < this.width && z < this.height && fields[(int) x][(int) z].getGameEntity() != null) {
+            if (x > 0 && z > 0 && x < this.width && z < this.height) {
+                if (fields[(int) x][(int) z].getGameEntity().isRigidBody()) return true;
+            }
         }
         return false;
     }
@@ -69,11 +66,9 @@ public class GroundMap extends UniqueGameEntity {
         texturedModel.setTextureStretch(this.fields.length);
     }
 
-    public void initGame(){
+    public void initGame() {
         /* TODO */
     }
-
-
 
 
 }
