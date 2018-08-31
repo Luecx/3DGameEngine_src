5f862abf8dedfc01ad7de134c1e16b92ec4b218d
Leon <leon.knott@hotmail.de>
2018-08-31 15:18:07 +0200

    update

--- projects/labyrinth/labyrinth_2D.java
+++ projects/labyrinth/labyrinth_2D.java
@@ -9,20 +9,78 @@ import engine.linear.loading.Loader;
 import engine.linear.loading.OBJLoader;
 import engine.linear.material.EntityMaterial;
 
+import java.awt.*;
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class labyrinth_2D extends Labyrinth {
 
     private int size;
     private boolean[][] wall;
 
+    //konstruktor
     public labyrinth_2D(int a) {
         this.size = a;
-        this.wall = new boolean[a][a];
+        this.wall = new boolean[a * 2 + 1][a * 2 + 1];
+
+    }
+
+    public boolean unvisitedCellsLeft(boolean[][] visited) {
+        for (int i = 1; i < this.wall.length - 1; i += 2) {
+            for (int k = 1; k < this.wall.length - 1; k += 2) {
+                if (visited[i][k] == false) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public Point getUnvisitedNeighbour(boolean[][] visited, Point cell) {
+        ArrayList<Point> neighbour = new ArrayList<Point>();
+        int[][] a = {{2, 0}, {0, 2}, {-2, 0}, {0, -2}};
+        for (int[] dir : a) {
+
+            try {
+                if (visited[cell.x + dir[0]][cell.y + dir[1]] == false) {
+                    neighbour.add(new Point(cell.x + dir[0], cell.y + dir[1]));
+                }
+            } catch (Exception x) {
+
+            }
+
+
+        }
+        if (neighbour.size() == 0) {
+            return null;
+        } else {
+            return neighbour.get((int) (Math.random() * neighbour.size()));
+        }
+
     }
 
     public void generate() {
-        this.wall[3][3] = true;
+        for (int i = 0; i < this.wall.length; i++) {
+            for (int k = 0; k < this.wall.length; k++) {
+                this.wall[i][k] = true;
+            }
+        }
+        for (int i = 1; i < this.wall.length - 1; i += 2) {
+            for (int k = 1; k < this.wall.length - 1; k += 2) {
+                this.wall[i][k] = false;
+            }
+        }
+        this.wall[0][(int) (Math.random() * size) * 2 + 1] = false;
+        this.wall[this.wall.length - 1][(int) (Math.random() * size) * 2 + 1] = false;
+        boolean[][] visited = new boolean[size * 2 + 1][size * 2 + 1];
+        Stack<Point> stack = new Stack<>();
+        while (unvisitedCellsLeft(visited) == true) {
+            if () {
+
+            } else {
+
+            }
+        }
     }
 
 
@@ -39,12 +97,12 @@ public class labyrinth_2D extends Labyrinth {
         TexturedModel model = new TexturedModel(rawModel, material);
         model.setTextureStretch(1);
 
-        for(int i = 0; i < size; i++){
-            for(int n = 0; n<  size; n++){
-                if(wall[i][n]){
+        for (int i = 0; i < this.wall.length; i++) {
+            for (int n = 0; n < this.wall.length; n++) {
+                if (wall[i][n]) {
                     Entity e = new Entity(model);
-                    e.setScale(0.5f,2,0.5f);
-                    e.setPosition(i,0,n);
+                    e.setScale(0.5f, 2, 0.5f);
+                    e.setPosition(i, 0, n);
                     arrayList.add(e);
                 }
             }
