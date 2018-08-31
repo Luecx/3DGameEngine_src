5b315679d403f9c0b69ecfc14633f69ae4261f02
Unknown <finn.eggers@gmx.de>
2018-08-31 13:08:51 +0200

    123

--- projects/labyrinth/Game.java
+++ projects/labyrinth/Game.java
@@ -1,13 +1,30 @@
 package projects.labyrinth;
 
+import engine.core.components.Light;
+import engine.core.components.PerspectiveCamera;
 import engine.core.exceptions.CoreException;
 import engine.core.master.RenderCore;
+import engine.core.master.RenderSettings;
+import engine.core.sourceelements.RawModel;
 import engine.core.system.Sys;
 import engine.linear.entities.Entity;
+import engine.linear.entities.TexturedModel;
+import engine.linear.loading.Loader;
+import engine.linear.loading.OBJLoader;
+import engine.linear.material.EntityMaterial;
+import engine.linear.material.Material;
+import engine.linear.material.SkydomeElement;
+import projects.buggy_project.Parameter;
+
+import java.util.ArrayList;
 
 public class Game extends RenderCore {
 
     private Labyrinth labyrinth;
+
+    private PerspectiveCamera perspectiveCamera;
+    private Entity ground;
+    private ArrayList<Light> lights = new ArrayList<Light>();
 
     public Game(Labyrinth labyrinth) {
         this.labyrinth = labyrinth;
@@ -16,7 +33,6 @@ public class Game extends RenderCore {
     @Override
     protected void onEnable() {
         Sys.enableAll();
-
         labyrinth.generate();
         for(Entity e:labyrinth.getModel()){
             try {
@@ -25,6 +41,40 @@ public class Game extends RenderCore {
                 e1.printStackTrace();
             }
         }
+
+        perspectiveCamera = new PerspectiveCamera();
+        perspectiveCamera.setPosition(10,10,10);
+
+        lights.add(new Light(1000,10000,3000));
+
+        RawModel rawModel = OBJLoader.loadOBJ("models/env", false);
+        EntityMaterial material = new EntityMaterial(Loader.loadTexture("textures/colormaps/grass"));
+        TexturedModel model = new TexturedModel(rawModel, material);
+        model.setTextureStretch(1000);
+        Entity e = new Entity(model);
+        e.setScale(1000,1,1000);
+        try {
+            Sys.ENTITY_SYSTEM.addElement(e);
+        } catch (CoreException e1) {
+            e1.printStackTrace();
+        }
+
+
+        SkydomeElement element = new SkydomeElement(Loader.loadTexture("textures/colormaps/sky"));
+        RenderSettings.skydome_use_skysphere = false;
+        RenderSettings.skydome_radius = 30000;
+        RenderSettings.skydome_follow_x_axis = true;
+        RenderSettings.skydome_follow_z_axis = true;
+        RenderSettings.skydome_follow_y_axis = false;
+        RenderSettings.skydome_bounding_y_axis = 000;
+        RenderSettings.skydome_fog_midlevel = 3000;
+        RenderSettings.skydome_fog_gradient = 0.3f;
+        try {
+            Sys.SKYDOME_SYSTEM.addElement(element);
+        } catch (CoreException e1) {
+            e1.printStackTrace();
+        }
+
     }
 
     @Override
@@ -34,11 +84,13 @@ public class Game extends RenderCore {
 
     @Override
     protected void render() {
-
+        Sys.ENTITY_SYSTEM.render(lights, perspectiveCamera);
+        Sys.SKYDOME_SYSTEM.render(perspectiveCamera);
+        perspectiveCamera.move();
     }
 
 
     public static void main(String[] args){
-        Game g = new Game();
+        Game g = new Game(new labyrinth_2D(10));
     }
 }
--- projects/labyrinth/labyrinth_2D.java
+++ projects/labyrinth/labyrinth_2D.java
@@ -1,6 +1,13 @@
 package projects.labyrinth;
 
+import engine.core.exceptions.CoreException;
+import engine.core.sourceelements.RawModel;
+import engine.core.system.Sys;
 import engine.linear.entities.Entity;
+import engine.linear.entities.TexturedModel;
+import engine.linear.loading.Loader;
+import engine.linear.loading.OBJLoader;
+import engine.linear.material.EntityMaterial;
 
 import java.util.ArrayList;
 
@@ -16,7 +23,6 @@ public class labyrinth_2D extends Labyrinth {
 
     public void generate() {
         this.wall[3][3] = true;
-
     }
 
 
@@ -26,6 +32,24 @@ public class labyrinth_2D extends Labyrinth {
 
 
     public ArrayList<Entity> getModel() {
-        return null;
+        ArrayList<Entity> arrayList = new ArrayList<>();
+
+        RawModel rawModel = OBJLoader.loadOBJ("models/cube", false);
+        EntityMaterial material = new EntityMaterial(Loader.loadTexture("textures/colormaps/redpng"));
+        TexturedModel model = new TexturedModel(rawModel, material);
+        model.setTextureStretch(1);
+
+        for(int i = 0; i < size; i++){
+            for(int n = 0; n<  size; n++){
+                if(wall[i][n]){
+                    Entity e = new Entity(model);
+                    e.setScale(0.5f,2,0.5f);
+                    e.setPosition(i,0,n);
+                    arrayList.add(e);
+                }
+            }
+        }
+
+        return arrayList;
     }
 }
