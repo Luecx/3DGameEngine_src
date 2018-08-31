bb23b7c079d462eda955a07ba5710c612371cd1d
Leon <leon.knott@hotmail.de>
2018-08-31 12:37:25 +0200

    ok

---
+++ projects/labyrinth/labyrinth_2D.java
@@ -0,0 +1,22 @@
+package projects.labyrinth;
+
+import engine.linear.entities.Entity;
+
+import java.util.ArrayList;
+
+public class labyrinth_2D extends Labyrinth {
+    @Override
+    public void generate() {
+        
+    }
+
+    @Override
+    public boolean collides(float x, float y) {
+        return false;
+    }
+
+    @Override
+    public ArrayList<Entity> getModel() {
+        return null;
+    }
+}
